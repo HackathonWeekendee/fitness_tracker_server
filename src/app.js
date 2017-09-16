c557cf2765cfcab85f37231390d4de78413889ff
Hackathon <Hackathon.Weekender@gmail.com>
2017-09-16 12:29:05 +0530

    Getting single user. Editor config.

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 const config = require('../config')
-
+const mongojs = require('mongojs')
 const app = express()
+
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
@@ -12,10 +13,10 @@ app.use(cors())
 /* connecting to database on m-lab */
 const dbUrl = `${config.db.options.protocol}${config.db.username}:${config.db.password}${config.db.options.qualifier}`
 const collections = config.db.options.collections
-const db = require('mongojs')(dbUrl, collections)
+const db = mongojs(dbUrl, collections)
 
 /* sample api */
-app.get('/getUsers', (req, res) => {
+app.get('/users', (req, res) => {
   db.user.find((err, users) => {
     if (err || !users) {
       console.error('Did not receive users')
@@ -24,6 +25,52 @@ app.get('/getUsers', (req, res) => {
       res.send(users)
     }
   })
+})
+
+app.get('/user/:user_id', (req, res) => {
+  const userId = req.params['user_id']
+  db.user.findOne({
+    '_id': mongojs.ObjectId(userId)
+  }, (err, user) => {
+    if (err || !user) {
+      const error = `Cannot find user with given id: ${userId}`
+      console.error(error)
+      res.end({
+        message: error
+      })
+    } else {
+      console.log(`User fetched for id ${userId}: ${user}`)
+      res.send(user)
+    }
+  })
+})
+
+app.post('/user', (req, res) => {
+  const user = {
+    name: req.body.name,
+    age: req.body.age,
+    dob: req.body.dob,
+    height: req.body.height,
+    weight: req.body.weight,
+    step_target: req.body.step_target,
+    cycling_target: req.body.cycling_target,
+    swim_target: req.body.swim_target
+  }
+  console.log(user)
+
+  const error = `Failed at user creation.`
+
+  db.user.save(
+    user, (err, saved) => {
+      if (err || !saved) {
+        console.error(error)
+        res.send({
+          message: error
+        })
+      } else {
+        res.send('User saved!')
+      }
+    })
 })
 
 app.listen(8001)
---
+++ src/user.model.js
@@ -0,0 +1,14 @@
+class User {
+  constructor (name, age, dob, height, weight, stepTarget, cyclingTarget, swimTarget) {
+    this.name = name
+    this.age = age
+    this.dob = dob
+    this.height = height
+    this.weight = weight
+    this.step_target = stepTarget
+    this.cycling_target = cyclingTarget
+    this.swim_target = swimTarget
+  }
+}
+
+module.exports = User
